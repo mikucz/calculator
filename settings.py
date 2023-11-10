d895f42616130bcaab5d3e7303992226d22ce0ac
Michal Kuczaj <michalxkuczaj@gmail.com>
2023-11-10 12:55:07 +0100

    chore: add django rest framework and drf-spectacular

--- settings.py
+++ settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "calculator",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,12 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
